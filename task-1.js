642f05399357b8aa27ac9940bf04400d54944b61
MD ABDULLAH <mdabdullah161036@gmail.com>
2025-08-08 18:26:59 +0600

    task-1 solved

---
+++ task-1.js
@@ -0,0 +1,10 @@
+// Task-1
+// Take four parameters. Multiply the four numbers and then return the result
+
+function multiply(a, b, c, d) {
+  const multiply = a * b * c * d;
+  return multiply;
+}
+
+const result = multiply(2, 3, 4, 5);
+console.log(result);
